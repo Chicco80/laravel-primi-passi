46ec0a705d2c29190d6aed48f2b2ce674b35b2bb
Chicco80 <chicco80lisa@gmail.com>
2022-06-09 18:12:08 +0200

    fine

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,19 +4,34 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Home</title>
     
 
 </head>
 <body>
-    <div class="container">
-        <h1>Ciao Mondo</h1>
-        <ul>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Ciao Mondo</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+       <div class="navbar-nav">
             @foreach($links as $link)
-            <li><a href="{{route($link)}}">{{$link}}</a></li>
+                <li class="px-5"><a href="{{route($link)}}">{{$link}}</a></li>
             @endforeach
-        </ul>
+      </div>
     </div>
+  </div>
+</nav>
 </body>
 </html>
+
+<style>
+
+    li{
+        list-style-type: none;
+    }
+</style>
 
